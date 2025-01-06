5cb9f4dbee263bd7093f1dd06f8301e5f1c9bfc7
rosericazondekon <roseric_2000@yahoo.fr>
2025-01-06 00:56:01 -0500

    Clean Home controller

--- MVCapp/Controllers/HomeController.cs
+++ MVCapp/Controllers/HomeController.cs
@@ -23,21 +23,6 @@ public class HomeController : Controller
         return View();
     }
 
-    // public IActionResult Feedback()
-    // {
-    //     return View();
-    // }
-
-    [HttpPost]
-    public IActionResult SubmitFeedback(string userName, string userFeedback)
-    {
-        // Process the feedback data (e.g., save to a database)
-        // Simulate server processing delay for demonstration purposes
-        System.Threading.Thread.Sleep(2000);
-        return Json(new { success = true, message = "Feedback submitted successfully!" });
-    }
-
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
